359aa64220c082b1f81d6c6f10a99e33f442e5cc
Willie Tong <tongsinyin@gmail.com>
2016-02-11 13:15:16 +0800

    solutions

--- js/smart-plant-eater.js
+++ js/smart-plant-eater.js
@@ -1,9 +1,25 @@
-function SmartPlantEater() {};
+function SmartPlantEater() {
+  this.energy = 30;  
+  this.direction = "e";
+}
 
 SmartPlantEater.prototype.act = function(view) {  
-  // This is just a dummy behavior
+  var space = view.find(" ");  
+  if (this.energy > 90 && space)    
+    return {type: "reproduce", direction: space};  
+
+  var plants = view.findAll("*");  
+  if (plants.length > 1)    
+    return {
+      type: "eat", 
+      direction: randomElement(plants)
+    };
+
+  if (view.look(this.direction) != " " && space)    
+    this.direction = space;  
+
   return {
     type: "move", 
-    direction: "e"
+    direction: this.direction
   };
 };
